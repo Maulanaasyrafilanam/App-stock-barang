198fae2c7dd49883e938f6c0ae67442198afe1fa
Maulanaasyrafilanam <87605565+Maulanaasyrafilanam@users.noreply.github.com>
2021-07-18 19:34:33 +0700

    Add files via upload
    Aplikasi untuk memeriksa persediaan/stock suatu barang ,pemasukkan barang dan pengeluaran barang dalam suatu toko atau tempat penyimpanan barang.

---
+++ stock.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 18, 2021 at 02:15 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stockbarang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `idbarang` int(11) NOT NULL,
+  `namabarang` varchar(50) NOT NULL,
+  `deskripsi` varchar(50) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`idbarang`, `namabarang`, `deskripsi`, `stock`) VALUES
+(1, 'laptop', 'merk lenovo', 18),
+(2, 'kable', 'usb tipe c', 8),
+(3, 'crimping ', 'alat untuk memasang kabel', 12),
+(4, 'RAM', 'memori internal', 14),
+(5, 'router', 'untuk menghubungkan jarin', 5),
+(6, 'flashdisk', 'sandisk 32 gb', 8),
+(7, 'hardisk', 'samsung 1 TB', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idbarang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
